bd65d951c0da9669d54a048d19e4ae1d348e01c0
kohei-tofu <fukouhei001@gmail.com>
2021-11-24 21:55:01 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import ImageHandler from './components/imagehandler/ImageHandler'
+import ImageHandlerSlider from './components/imagehandler/ImageHandlerSlider'
+
+import { URL_HOST, API_POST, TITLE, DIALOG_TITLE, DIALOG_TEXTCONTENT } from './config'
 
 function App() {
+
+  const stateSelective = [{name: 'Style-1', value: 0, use:true}, 
+                          {name: 'Style-2', value: 1, use:true},
+                          {name: 'Style-3', value: 2, use:false},
+                          {name: 'Style-4', value: 3, use:false}
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageHandlerSlider url_host={URL_HOST} api_post={API_POST} 
+                          title={TITLE} 
+                          dialog_title = {DIALOG_TITLE} dialog_textcontent={DIALOG_TEXTCONTENT}
+                          initAlpha = {0.2}
+                          sSelective = {stateSelective}
+      />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+console.log(process.env)
+// exports.HOST_URL = process.env.API_URL || "http://localhost:1112"
+// exports.API_POST = process.env.API || "keypoint/image-raw"
+
+// exports.URL_HOST = process.env.URL_HOST || "http://localhost:5555"
+exports.URL_HOST = process.env.URL_HOST || "https://mykeypointserver.kevin-asobi.com"
+
+exports.API_POST = process.env.API_POST || "image"
+
+exports.TITLE = 'Style-Changer'
+exports.DIALOG_TITLE = 'Choose a photo that you would like to process.'
+exports.DIALOG_TEXTCONTENT = 'Image-Button to choose picture is on your left-side. This module is going to change styles after your submitting'
+
