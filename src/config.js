785e9781ea216e7760ce0b91a6c2cb6aa9e10230
kohei <koheitech001@gmail.com>
2022-10-29 23:37:00 +0900

    update sub

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-// console.log(process.env)
-// exports.HOST_URL = process.env.API_URL || "http://localhost:1112"
-// exports.API_POST = process.env.API || "keypoint/image-raw"
-
-// exports.URL_HOST = process.env.URL_HOST || "http://localhost:5555"
-exports.URL_HOST = process.env.URL_HOST || "https://apistransfer.kevin-asobi.com"
-
+exports.URL_HOST = process.env.URL_HOST || "https://styletransfer-server"
 exports.API_POST = process.env.API_POST || "image"
 
 exports.TITLE = 'Style-Changer'
